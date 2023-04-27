bd059f8c1cfdd7625633bfb390a7407fa7f6d45b
alpezel <allistairevillaruel@gmail.com>
2023-04-27 21:09:46 +0800

    make txt file and ask user for input and test run

---
+++ my_life.py
@@ -0,0 +1,13 @@
+#Write a method in python to write multiple line of text
+#contents into a text file mylife.txt. See sample output:
+#Enter line: Beautiful is better than ugly.
+#Are there more lines y/n? y
+#Enter line: Explicit is better than implicit.
+#Are there more lines y/n? y
+#Enter line: Simple is better than complex.
+#Are there more lines y/n? n 
+
+#ask user to input line
+my_line = input("Enter line:")
+#test if working
+print (my_line)
