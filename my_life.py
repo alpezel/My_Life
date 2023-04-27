0950c727c7c0e0745710d26c53ee10a79c7f32a1
alpezel <allistairevillaruel@gmail.com>
2023-04-27 21:13:26 +0800

    open txt file and test if the inputted line is added to txt file

--- my_life.py
+++ my_life.py
@@ -9,5 +9,10 @@
 
 #ask user to input line
 my_line = input("Enter line:")
+
+# open txt file where the line will be appended
+with open("My_Life.txt","a") as my_life_file:
+    my_life_file.write(my_line)
+
 #test if working
-print (my_line)
+print ("lines added in txt file:",my_line)
