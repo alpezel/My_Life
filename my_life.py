406d12007de921866ecc2c4c57389fd769b6b085
alpezel <allistairevillaruel@gmail.com>
2023-04-27 21:20:02 +0800

    add def() to the code for easier calling later when making loop for trying again to input new line

--- my_life.py
+++ my_life.py
@@ -7,12 +7,17 @@
 #Enter line: Simple is better than complex.
 #Are there more lines y/n? n 
 
-#ask user to input line
-my_line = input("Enter line:")
 
-# open txt file where the line will be appended
-with open("My_Life.txt","a") as my_life_file:
-    my_life_file.write(my_line)
+def Add_line():
+    #ask user to input line
+    my_line = input("Enter line:")
+    # open txt file where the line will be appended
+    with open("My_Life.txt","a") as my_life_file:
+        my_life_file.write(str(my_line) +"\n")
 
-#test if working
-print ("lines added in txt file:",my_line)
+    #test if working
+    print ("lines added in txt file:",my_line)
+  
+Add_line()
+
+
